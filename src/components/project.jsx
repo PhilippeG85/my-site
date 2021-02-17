e45cca04273d19e17b74e1c24439d480a2a183f1
Philippe Gonse <pgonse@gmail.com>
2021-02-17 11:58:05 -0500

    add photo to projects

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
 
-function Project({ title, link, receivedIcon }) {
+function Project({
+  title,
+  link,
+  receivedIcon,
+  back
+}) {
   const [display, setDisplay] = useState("none");
+  const [backgroundImage, setBackgroundImage] = useState(`url("${back}")`);
 
   const icon = receivedIcon.map((i) => {
-    return <img src={i} alt="" className="project-img" />;
+    return <img src={i} alt="" className="project-img" key={i} />;
   });
 
+  const handleEnter = () => {
+    setDisplay('block');
+    setBackgroundImage(`linear-gradient(rgba(255,255,255,.4), rgba(255,255,255,.4)), url("${back}")`);
+  };
+
+  const handleLeave = () => {
+    setDisplay("none");
+    setBackgroundImage(`url("${back}")`);
+  };
+
   return (
-    <div className="project" onMouseEnter={() => setDisplay("block")} onMouseLeave={() => setDisplay("none")}>
+    <div
+      className="project"
+      style={{ backgroundImage }}
+      onMouseEnter={() => handleEnter()}
+      onMouseLeave={() => handleLeave()}
+    >
       <div className="project-info" style={{ display }}>
         <h1 className="project-name">{title}</h1>
         <a href={link} target="_blank" rel="noreferrer" className="project-link">View Project</a>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,13 +8,31 @@ import react from "../assets/img/react.png";
 import js from "../assets/img/js.png";
 import firebase from "../assets/img/firebase.png";
 import netlify from "../assets/img/netlify.png";
+import lestroismats from "../assets/img/lestroismats.jpg";
+import website from "../assets/img/website.jpg";
+import helper from "../assets/img/helper.png";
 
 function Projects() {
   return (
     <div className="projects-name">
-      <Project title="Les Trois Mats" link="les-trois-mats.com" receivedIcon={[css, react, github, netlify]} />
-      <Project title="Philippe Gonse" link="#" receivedIcon={[css, react, github, firebase]} />
-      <Project title="Helper" link="helper.solutions" receivedIcon={[css, rails, js, github]} />
+      <Project
+        title="Les Trois Mats"
+        link="https://www.les-trois-mats.com"
+        receivedIcon={[css, react, github, netlify]}
+        back={lestroismats}
+      />
+      <Project
+        title="Philippe Gonse"
+        link="#"
+        receivedIcon={[css, react, github, firebase]}
+        back={website}
+      />
+      <Project
+        title="Helper"
+        link="http://www.helper.solutions/"
+        receivedIcon={[css, rails, js, github]}
+        back={helper}
+      />
     </div>
   );
 }
